ac869d22bd549dce4400db3c17e56fdeaa414887
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2017-09-14 22:35:04 +0100

    major updates

--- emd.py
+++ emd.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import interpolate as interp
+from scipy import signal
 
 
 ## a peak is a sign change in the differential of a signal, we want to find
 # adjacent samples in the diff with different signs
 
-def new_find_envelopes( t, X ):
+def find_envelopes( t, X, to_plot=False ):
 
     max_locs,max_pks = find_extrema( X[:,0] )
     min_locs,min_pks = find_extrema( X[:,0], ret_min=True)
@@ -14,7 +15,7 @@ def new_find_envelopes( t, X ):
     if max_locs.size < 2 or min_locs.size < 2:
         return [],[]
 
-    N = 50 # should make this analytic somehow
+    N = 2 # should make this analytic somehow
 
     ret_max_locs = np.pad( max_locs,N,'reflect',reflect_type='odd')
     ret_min_locs = np.pad( min_locs,N,'reflect',reflect_type='odd')
@@ -29,6 +30,10 @@ def new_find_envelopes( t, X ):
 
     t = np.arange(ret_max_locs[0],ret_max_locs[-1])
     tinds = np.logical_and((t >= 0), (t < X.shape[0]))
+    if to_plot:
+        print tinds.sum()
+        print t.shape
+        print upper.shape
     upper = upper[tinds]
 
     f = interp.splrep( ret_min_locs, ret_min_pks )
@@ -36,23 +41,37 @@ def new_find_envelopes( t, X ):
 
     t = np.arange(ret_min_locs[0],ret_min_locs[-1])
     tinds = np.logical_and((t >= 0), (t < X.shape[0]))
+    if to_plot: print tinds.sum()
     lower = lower[tinds]
+
+    if to_plot:
+        plt.plot(X[:,0])
+        plt.plot(ret_max_locs,ret_max_pks,'*')
+        plt.plot(max_locs,max_pks,'o')
+        plt.plot(ret_min_locs,ret_min_pks,'*')
+        plt.plot(min_locs,min_pks,'o')
+        plt.plot(upper)
+        plt.plot(lower)
+        plt.show()
 
     return upper, lower
 
 def find_extrema( X, ret_min=False ):
+
+    if ret_min:
+        ind = signal.argrelextrema( X, np.less_equal)[0]
+    else:
+        ind = signal.argrelextrema( X, np.greater_equal)[0]
+
+    return ind, X[ind]
+
+def find_extrema3( X, ret_min=False ):
 
     dX = np.diff( X )
 
     # Finds anywhere there is a sign change in the diff
     inds = np.where(np.sign(dX[1:]*dX[:-1]) < 0)[0] + 1
 
-    # Find sign differences between adjacent extrema
-    #d_ext = np.sign(np.diff(X[inds]))
-    #if d_ext[-1] == 1:
-    #    d_ext = np.concatenate( (d_ext,[1]) )
-    #else:
-    #    d_ext = np.concatenate( (d_ext,[-1]) )
     d_ext = np.sign(dX[inds])
 
     # ID max or min
@@ -68,194 +87,20 @@ def find_extrema( X, ret_min=False ):
     else:
         return ext_ind, X[ext_ind]
 
-def interpolate_envelope( locs, peaks, pad, nsamples ):
-    """ Designed to work on indices not time values """
+def sift( X, imf_thresh=1e-4, sift_thresh=1e-8 ):
 
-    #f = interp.interp1d( locs, peaks, kind='cubic')
-    #f = interp.InterpolatedUnivariateSpline( locs, peaks, ext=3 )
-
-    f = interp.splrep( locs, peaks )
-    spl = interp.splev(range(locs[0],locs[-1]), f)
-
-    print spl.shape
-    print pad,nsamples
-
-    #ret = np.nan * np.zeros( nsamples, )
-    #ret = np.zeros( nsamples, )
-
-    #ret[locs[0]:locs[-1]] = f(range(locs[0],locs[-1]))
-
-    #return f(range(0,nsamples))
-    return spl[pad:pad+nsamples]
-
-##
-
-# reflect upper and lower points and locs around axis
-# append to existing upper and lower points and locs
-
-def extend_extrema2( min_loc, min_pk, max_loc, max_pk, N=3 ):
-
-    # Find the axis of symmetry at start and end of extrema
-    start_symm = np.min( (min_loc[0],max_loc[0]) )
-    end_symm = np.max( (min_loc[-1],max_loc[-1]) )
-
-    # Check whether the final point is in the upper or lower extrema, we don't
-    # want to reflect the final point
-    start_minima = False
-    if start_symm == min_loc[0]:
-        start_minima = True
-
-    end_minima = False
-    if end_symm == min_loc[0]:
-        end_minima = True
-
-    # Minima
-
-    # If the first extrema is a trough, skip it here
-    if start_minima:
-        inds = range(1,N+1)
-    else:
-        inds = range(0,N)
-    print inds
-    # Do the reflection
-    p_locs = reflect_points( min_loc[inds], origin=start_symm )
-    p_pks = reflect_points( min_pk[inds], origin=start_symm )
-
-    # If the final extream is a trough, don't try to reflect it
-    if end_minima:
-        inds = range(min_loc.shape[0]-N-2,min_loc.shape[0]-2)
-    else:
-        inds = range(min_loc.shape[0]-N-1,min_loc.shape[0]-1)
-    print inds
-    # Do the reflection
-    a_locs = reflect_points( min_loc[inds], origin=end_symm, direction='right' )
-    a_pks = reflect_points( min_pk[inds], origin=end_symm, direction='right' )
-    print end_symm
-    print a_pks
-
-    ret_min_locs = np.concatenate( (p_locs,min_loc,a_locs) )
-    ret_min_pks = np.concatenate( (p_pks,min_pk,a_pks) )
-
-    # Maxima
-
-    # If the first extrema is a trough, skip it here
-    if start_minima:
-        inds = range(1,N+1)
-    else:
-        inds = range(0,N)
-    print inds
-    # Do the reflection
-    p_locs = reflect_points( max_loc[inds], origin=start_symm )
-    p_pks = reflect_points( max_pk[inds], origin=start_symm )
-
-    # If the final extream is a trough, don't try to reflect it
-    if end_minima:
-        inds = range(max_loc.shape[0]-N-2,max_loc.shape[0]-2)
-    else:
-        inds = range(max_loc.shape[0]-N-1,max_loc.shape[0]-1)
-    print inds
-    # Do the reflection
-    a_locs = reflect_points( max_loc[inds], origin=end_symm, direction='right' )
-    a_pks = reflect_points( max_pk[inds], origin=end_symm, direction='right' )
-
-    ret_max_locs = np.concatenate( (p_locs,max_loc,a_locs) )
-    ret_max_pks = np.concatenate( (p_pks,max_pk,a_pks) )
-
-    return ret_min_locs,ret_min_pks,ret_max_locs,ret_min_pks
-
-def find_envelopes( time_vect, X ):
-
-    [max_loc,max_pk] = find_extrema( X[:,0] )
-    [min_loc,min_pk] = find_extrema( X[:,0], ret_min=True)
-
-    if np.array(max_loc).size < 2 or np.array(min_loc).size < 2:
-        return None,None
-
-    # extend maxima
-    #if len(max_loc) > 3:
-    #    max_loc_ext,max_pk_ext = extend_extrema( max_loc,max_pk )
-    #else:
-    #    max_loc_ext,max_pk_ext = extend_extrema( max_loc,max_pk, N=1 )
-
-    #if len(min_loc) > 3:
-    #    min_loc_ext,min_pk_ext = extend_extrema( min_loc,min_pk )
-    #else:
-    #    min_loc_ext,min_pk_ext = extend_extrema( min_loc,min_pk, N=1 )
-
-    if len(min_loc) > 3:
-         min_loc_ext,min_pk_ext,max_loc_ext,max_pk_ext = extend_extrema2( min_loc, min_pk, max_loc, max_pk )
-    else:
-         min_loc_ext,min_pk_ext,max_loc_ext,max_pk_ext = extend_extrema2( min_loc, min_pk, max_loc, max_pk, N=1 )
-
-    max_pad = max_loc[0] + np.abs(max_loc_ext[0])
-    min_pad = min_loc[0] + np.abs(min_loc_ext[0])
-
-    max_env = interpolate_envelope( max_loc_ext, max_pk_ext, max_pad, X.shape[0] )
-    min_env = interpolate_envelope( min_loc_ext, min_pk_ext, min_pad, X.shape[0] )
-
-    print max_env.shape
-    print min_env.shape
-
-    return max_env,min_env
-
-def reflect_points( points, direction='left', origin=0 ):
-
-    if direction == 'left':
-        # Compute step to points from origin
-        steps =  np.cumsum(np.diff(np.concatenate( ([origin],points) )))
-        return np.array(origin-steps-1)[::-1]
-
-    elif direction == 'right':
-        # Compute step to points from origin
-        a = np.concatenate( (points,[origin]) )
-        print a
-        steps =  np.cumsum(np.diff(np.concatenate( (points,[origin]) )[::-1]))
-        return np.array(origin-steps+1)
-
-    else:
-        print 'Direction %s not recognised' % direction
-
-def extend_extrema( locs, pks, N=3 ):
-
-    # peaks to prepend
-    d = np.diff(locs)
-    p_locs = np.array(locs[0] - np.cumsum( d[:N][::-1] ))
-    print locs
-    print p_locs
-    p_pks = pks[1:N+1]
-
-    # peaks to append
-    a_locs = locs[-1] + np.cumsum(d[-N:][::-1])
-    a_pks = pks[-N-1:-1][::-1]
-
-    # full lists
-    ret_locs = np.concatenate( (p_locs[::-1],locs,a_locs) )
-    ret_pks = np.concatenate( (p_pks[::-1],pks,a_pks) )
-
-    return ret_locs,ret_pks
-
-if __name__ == '__main__':
-
-    t = np.linspace(0,10,15000)
-    X = np.sin( 2*np.pi*4*t) + np.sin( 2*np.pi*14*t) + np.random.randn(15000,)*.02 + np.linspace(-1,1,15000)
-    
-    sift_thresh = 1e-12
-    imf_thresh = 1e-6
-    
-    X2 = X.copy()
-    
     continue_sift = True
     layer = 0
     while continue_sift:
         print layer
-    
+
         # initialise
         if layer == 0:
-            proto_imf = X2[:,None].copy()
+            proto_imf = X[:,None].copy()
             imf = np.zeros_like( proto_imf )
         else:
-            proto_imf = X2[:,None] - np.sum(imf,axis=1)[:,None]
-    
+            proto_imf = X[:,None] - np.sum(imf,axis=1)[:,None]
+
         continue_imf = True
         itera = 0
 
@@ -263,34 +108,92 @@ if __name__ == '__main__':
             continue_sift = False
 
         while continue_imf:
-    
-            upper,lower = new_find_envelopes( t, proto_imf )
-    
+
+            upper,lower = find_envelopes( t, proto_imf )
+
             # If upper or lower are None we should stop sifting alltogether
             if upper is None or lower is None:
                 continue_sift=False
-    
+
                 if layer == 0:
                     imf = proto_imf
                 else:
                     imf = np.concatenate( (imf,proto_imf), axis=1 )
                 break
-    
+
             # Find average envelope
             avg = np.mean([upper,lower],axis=0)
-    
+
             # If the envelope has less power than our threshold, stop this imf
             if np.nansum(np.power(avg,2)) < imf_thresh:
                 print 'stopping this imf'
                 continue_imf=False
-    
+
                 if layer == 0:
                     imf = proto_imf
                 else:
                     imf = np.concatenate( (imf,proto_imf), axis=1 )
                 layer += 1
                 break
-    
+
             # We're going to keep sifting if we made it this far
             proto_imf = proto_imf - avg[:,None]
             itera += 1
+
+    return imf
+
+def instantaneous_stats( imf, sample_rate, method ):
+
+    infreq = np.zeros_like( imf )
+
+    if method == 'hilbert':
+
+        analytic_signal = signal.hilbert( imf, axis=0 )
+        instantaneous_phase = np.unwrap(np.angle(analytic_signal),axis=0)
+        instantaneous_frequency = (np.diff(instantaneous_phase,axis=0) / (2.0*np.pi) * sample_rate)
+
+        instantaneous_amp = np.abs(analytic_signal)
+
+    else:
+
+        print('Method not recognised')
+
+    return instantaneous_frequency, instantaneous_amp
+
+def hilberthuang( infr, inam, fbins ):
+
+    # add this later...
+    #tinds = np.digitize( time_vect, tbins )
+
+    hht = np.zeros( (infr.shape[0],len(fbins)+1) )
+
+    for ii in range(infr.shape[0]):
+
+        # Add frequency info for this time bin
+        finds = np.digitize( infr[ii,:], fbins )
+        hht[ii,finds] = inam[ii,:]
+
+    return hht
+
+
+if __name__ == '__main__':
+
+    sample_rate = 1000
+    seconds = 10
+    num_samples = seconds*sample_rate
+    t = np.linspace(0,seconds,num_samples)
+    #X = np.sin( 2*np.pi*4*t) + np.sin( 2*np.pi*14*t) + np.random.randn(num_samples,)*.02 + np.linspace(-1,1,num_samples)
+    X = np.r_[signal.chirp( t[:5000], 5, 5, 25 ), signal.chirp( t[5000:],25,10,5)] + np.sin( 2*np.pi*50*t )
+
+    imf = sift( X )
+
+    plt.figure()
+    plt.plot(imf)
+
+    infr, inam = instantaneous_stats( imf, sample_rate, 'hilbert' )
+    hht = hilberthuang( infr, inam, np.linspace(0,75,125) )
+
+    plt.figure()
+    plt.contourf( np.sqrt(hht.T) )
+
+    plt.show()
