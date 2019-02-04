8ebfb2ee543a28e7e5c4319710ee60980acbd298
Andrew Quinn <aquinn501@gmail.com>
2019-02-04 15:38:29 +0000

    Update to IMF plotting function

--- emd/plotting.py
+++ emd/plotting.py
@@ -1,22 +1,26 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import Colormap
 
-def plot_imfs(imfs, time_vect=None, scale_y=False):
+def plot_imfs(imfs, time_vect=None, scale_y=False, freqs=None,cmap=None ):
     """
+    Create a quick summary plot for a set of IMFs.
 
     Parameters
     ----------
-    imfs :
-
-    time_vect :
-         (Default value = None)
-    scale_y :
-         (Default value = False)
-
-    Returns
-    -------
-
+    imfs : ndarray
+        2D array of IMFs to plot
+    time_vect : ndarray
+         Optional 1D array specifying time values (Default value = None)
+    scale_y : Boolean
+         Flag indicating whether the y-axis should be adative to each mode
+         (False) or consistent across modes (True) (Default value = False)
+    freqs : array_like
+        Optional vector of frequencies for each IMF
+    cmap : {None,True,matplotlib colormap}
+        Optional colourmap to use. None will plot each IMF in black and True will
+        use the plt.cm.Dark2 colormap as default. A different colormap may also
+        be passed in.
 
     """
 
@@ -28,14 +32,39 @@ def plot_imfs(imfs, time_vect=None, scale_y=False):
 
     plt.figure()
     ax = plt.subplot(nplots, 1, 1)
+    for tag in ['top','right','bottom']:
+        ax.spines[tag].set_visible(False)
+    ax.plot( (time_vect[0],time_vect[-1]),(0,0),color=[.5,.5,.5])
     ax.plot(time_vect, imfs.sum(axis=1), 'k')
     ax.tick_params(axis='x', labelbottom='off')
+    ax.set_xlim(time_vect[0],time_vect[-1])
+    ax.set_ylabel('Signal',rotation=0,labelpad=10)
+
+    if cmap is True:
+        # Use default colormap
+        cmap = plt.cm.Dark2
+        cols = cmap(np.linspace(0,1,imfs.shape[1]+1))
+    elif isinstance( cmap, Colormap ):
+        # Use specified colormap
+        cols = cmap(np.linspace(0,1,imfs.shape[1]+1))
+    else:
+        # Use all black lines - this is overall default
+        cols = np.array([[0,0,0] for ii in range(imfs.shape[1]+1)])
 
     for ii in range(1, nplots):
         ax = plt.subplot(nplots, 1, ii+1)
-        ax.plot(time_vect, imfs[:, ii-1], 'k')
+        for tag in ['top','right','bottom']:
+            ax.spines[tag].set_visible(False)
+        ax.plot( (time_vect[0],time_vect[-1]),(0,0),color=[.5,.5,.5])
+        ax.plot(time_vect, imfs[:, ii-1], color=cols[ii,:])
+        ax.set_xlim(time_vect[0],time_vect[-1])
         if scale_y:
-            ax.set_ylim(-mx*1.2, mx*1.2)
+            ax.set_ylim(-mx*1.1, mx*1.1)
+        ax.set_ylabel('IMF {0}'.format(ii),rotation=0,labelpad=10)
 
         if ii < nplots:
             ax.tick_params(axis='x', labelbottom='off')
+        if freqs is not None:
+            ax.set_title(freqs[ii-1],fontsize=8)
+
+    plt.subplots_adjust(top=.95,bottom=.05,right=.95)
