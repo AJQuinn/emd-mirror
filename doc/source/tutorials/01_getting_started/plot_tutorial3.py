d8e5b9769cb1c718b52214c0b3d1adeca8b2d528
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2020-09-22 13:54:21 +0100

    Update advanced options in sift config tutorial

--- doc/source/tutorials/01_getting_started/plot_tutorial3.py
+++ doc/source/tutorials/01_getting_started/plot_tutorial3.py
@@ -16,14 +16,16 @@ all the way down to extrema detection.
 # config for a standard sift - we can view the options by calling print on the
 # config.
 #
-# The SiftConfig dictionary contains all the arguments for  functions that
+# The SiftConfig dictionary contains all the arguments for functions that
 # are used in the sift algorithm.
 #
 # - "sift" contains arguments for the high level sift functions such as ``emd.sift.sift`` or ``emd.sift.ensemble_sift``
 # - "imf" contains arguments for ``emd.sift.get_next_imf``
 # - "envelope" contains arguments for ``emd.sift.interpolate_envelope``
 # - "extrema", "mag_pad" and  "loc_pad" have arguments for extrema detection and padding
+
 import emd
+import numpy as np
 
 config = emd.sift.get_config('sift')
 print(config)
@@ -31,7 +33,7 @@ print(config)
 #%%
 # These arguments are specific for the each type of sift (particularly at the top "sift" level).
 
-config = emd.sift.get_config('ensemble_sift')
+config = emd.sift.get_config('complete_ensemble_sift')
 print(config)
 
 #%%
@@ -51,15 +53,17 @@ print(config)
 # the key-value pairs in a nested dictionary or using a h5py inspiried shorthand.
 
 # This is a top-level argument used directly by ensemble_sift
-config['nensembles'] = 24
+config['nensembles'] = 20
+config['nprocesses'] = 4
+config['max_imfs'] = 5
 
 # This is a sub-arguemnt used by interp_envelope, which is called within
 # ensemble_sift.
 
 # Standard
-config['envelope_opts']['interp_type'] = 'mono_pchip'
+config['extrema_opts']['pad_width'] = 4
 # Shorthard
-config['envelope_opts/interp_type'] = 'mono_pchip'
+config['extrema_opts/pad_width'] = 4
 
 print(config)
 
@@ -67,13 +71,97 @@ print(config)
 # This nested shorthand can be used to customise the low level extrema padding options
 
 # Standard
-config['extrema_opts']['loc_pad_opts']['reflect_type'] = 'even'
+#config['extrema_opts']['loc_pad_opts']['reflect_type'] = 'even'
 # Shorthand
 config['extrema_opts/mag_pad_opts/stat_length'] = 3
+config['extrema_opts'] = {}
 
 print(config)
 
-#%%
 # This nested structure is passed as an unpacked dictionary to our sift function.
 
-config = emd.sift.get_config('sift')
+# Create some random data
+x = np.random.randn(1000,)
+
+imf = emd.sift.complete_ensemble_sift(x, **config)
+
+#%%
+# Customised sifting with functools.partial
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# If you are going to be repeatedly calling a sift function with the same
+# arguments many times, you could consider creating a partial function to
+# simplify the code. Partial functions are a part of the ``functools`` module
+# in python. They act like normal functions but with fixed values for certain
+# arguments.
+#
+# This means we could specify our sift config and use it to create a partial
+# function which only needs the data to be passed in as an argument. For
+# example:
+
+# Create a mask sift config object and customise some options
+config = emd.sift.get_config('mask_sift')
+config['max_imfs'] = 5
+config['mask_amp_mode'] = 'ratio_sig'
+config['envelope_opts/interp_method'] = 'mono_pchip'
+
+# Create a partial function - my_mask_sift is now a function with the arguments
+# in config fixed as defaults.
+from functools import partial
+my_mask_sift = partial(emd.sift.mask_sift, **config)
+
+# my_mask_sift can then be called with the input data as the only argument.
+imfs = my_mask_sift(x)
+
+#%%
+# We can compare the different options for ``emd.sift.mask_sift`` and
+# ``my_mask_sift`` using the python inspect module to print the default
+# arguments (or function signature) for each function.
+
+import inspect
+print('emd.sift.mask_sift')
+print(inspect.signature(emd.sift.mask_sift))
+print()
+print('my_mask_sift')
+print(inspect.signature(my_mask_sift))
+
+#%%
+# We can see that the input arguments in the signature of ``my_mask_sift``
+# contains all the specified options from the ``config`` so is much longer than
+# for ``emd.sift.mask_sift``.
+
+#%%
+# Saving and loading sift config files
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# We might often want to store, reuse and share sift configurations during a
+# project. To help with this, a SiftConfig specification can be stored as a raw
+# text file in the YAML format. The config can be saved into a text file and
+# loaded back into a SiftConfig object for use in a script. We can also
+# directly edit the text file to customise the sift parameters from there if
+# preferred. The save and load operations are performed by
+# ``emd.sift.SiftConfig.to_yaml_file`` and
+# ``emd.sift.SiftConfig.from_yaml_file`` repectively.
+#
+# Lets look at an example. We're going to store this config in a temporary file
+# on your system for this tutorial. This avoids clutter and should work on all
+# systems. If you would prefer to use a specific file on your system please
+# comment out this section and simply specify ``config_file`` to be a path to
+# the file of your choice.
+
+# Create a temporary file OR specify your own file path
+import tempfile
+config_file = tempfile.NamedTemporaryFile(prefix="ExampleSiftConfig_").name
+# Or uncomment the following line and specify your own file
+#config_file = '/path/to/my/file'
+
+# Save the config into yaml format
+config.to_yaml_file(config_file)
+
+# Open the text file and print its contents
+with open(config_file, 'r') as f:
+    txt = f.read()
+print(txt)
+
+# Load the config back into a SiftConfig object for use in a script
+new_config = emd.sift.SiftConfig.from_yaml_file(config_file)
