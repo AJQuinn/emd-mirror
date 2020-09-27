01f5e2ae3322aded23478134ead0d7017e0332f4
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2020-09-27 16:09:42 +0100

    Major refactor of logger implementatin - now using dict_config and better, more specific helper functions

--- emd/logger.py
+++ emd/logger.py
@@ -2,83 +2,137 @@
 
 # vim: set expandtab ts=4 sw=4:
 
+import sys
+import yaml
 import numpy as np
 from functools import wraps
 from .support import get_install_dir, get_installed_version
 
+# Housekeeping for logging
 import logging
+import logging.config
+
+# Add a single null handler until set-up is called, this is activated on import
+# to __init__
+logging.getLogger('emd').addHandler(logging.NullHandler())
+
+# Initialise logging for this sub-module
 logger = logging.getLogger(__name__)
 
-INFO_fmt = '%(asctime)s {prefix} - %(funcName)10s() : %(message)s'
-INFO_date = '%H:%M:%S'
-DEBUG_fmt = '%(asctime)s {prefix} emd.%(module)s:%(lineno)s - %(funcName)20s() : %(message)s'
-DEBUG_date = '%m-%d %H:%M:%S'
+
+default_config = """
+version: 1
+loggers:
+  emd:
+    level: DEBUG
+    handlers: [console, file]
+    propagate: false
+
+handlers:
+  console:
+    class : logging.StreamHandler
+    formatter: default
+    level   : INFO
+    stream  : ext://sys.stdout
+  file:
+    class : logging.handlers.RotatingFileHandler
+    formatter: verbose
+    filename: {log_file}
+    backupCount: 3
+    maxBytes: 102400
+
+formatters:
+  brief:
+    format: '{prefix} %(message)s'
+  default:
+    format: '[%(asctime)s] {prefix} %(levelname)-8s %(funcName)14s : %(message)s'
+    datefmt: '%H:%M:%S'
+  verbose:
+    format: '[%(asctime)s] {prefix} - %(levelname)s - emd.%(module)s:%(lineno)s - %(funcName)20s() : %(message)s'
+    datefmt: '%Y-%m-%d %H:%M:%S'
+
+disable_existing_loggers: true
+
+"""
 
 
-def set_up(level='DEBUG', filename=None, mode='both', prefix=None):
+def set_up(prefix='', log_file=''):
     """
-    LEVELS = [DEBUG, INFO, WARN, ERROR, FATAL]
+    Initialisation for the EMD module logger.
+
+    Parameters
+    ----------
+    prefix : str
+        Optional prefix to attach to logger output
+    log_file : str
+        Optional path to a log file to record logger output
     """
 
-    if prefix is None:
-        prefix = ''
-    else:
-        prefix = '(' + prefix + ')'
+    # Format config with user options
+    new_config = default_config.format(prefix=prefix, log_file=log_file)
+    # Load config to dict
+    new_config = yaml.load(new_config, Loader=yaml.FullLoader)
 
-    if level == 'DEBUG':
-        fmt = DEBUG_fmt.format(prefix=prefix)
-        datefmt = DEBUG_date
-    else:
-        fmt = INFO_fmt.format(prefix=prefix)
-        datefmt = INFO_date
+    # Remove log file from dict if not user requested
+    if len(log_file) == 0:
+        new_config['loggers']['emd']['handlers'] = ['console']
+        del new_config['handlers']['file']
 
-    if (mode == 'console') or (mode == 'both'):
-        # Start logging to console
-        logging.basicConfig(level=getattr(logging, level),
-                            format=fmt,
-                            datefmt=datefmt)
+    # Configure logger with dict
+    logging.config.dictConfig(new_config)
 
-    if (filename is not None) and (mode == 'both'):
-        # Add file handler to existing console logger
-        filelog = logging.FileHandler(filename)
-        filelog.setLevel(level)
-        filelog.setFormatter(logging.Formatter(fmt))
-        # add the handler to the root logger
-        logging.getLogger('').addHandler(filelog)
-
-    if (filename is not None) and (mode == 'file'):
-        # Start logging to file
-        logging.basicConfig(level=getattr(logging, level),
-                            format=fmt, filename=filename,
-                            datefmt=datefmt)
-
-    logging.info('Logging Started on {0}'.format(level))
-    logging.info('Logging mode \'{0}\''.format(mode))
-    if (filename is not None) and (mode == 'file') or (mode == 'both'):
-        logging.info('Logging to file: {0}'.format(filename))
-
-    logging.info('EMD v{0} installed in {1}'.format(get_installed_version(),
+    # Print some info
+    logger.info('EMD Logger Started')
+    if len(log_file) > 0:
+        logger.info('logging to file: {0}'.format(log_file))
+    logger.debug('EMD v{0} installed in {1}'.format(get_installed_version(),
                                                     get_install_dir()))
 
 
-def set_level(level='INFO'):
-    """
-    LEVELS = [DEBUG, INFO, WARN, ERROR, FATAL]
-    """
-    logging.getLogger().setLevel(getattr(logging, level))
-    logging.info('Logging Level changed to {0}'.format(level))
+def set_level(level, handler='console'):
+    """Set new logging level for EMD module."""
+    logger = logging.getLogger('emd')
+    for handler in logger.handlers:
+        if handler.get_name() == 'console':
+            handler.setLevel(getattr(logging, level))
 
 
-def shut_down():
-    """
-    Function for disabling logging in EMD
+def get_level(handler='console'):
+    """Return current logging level for EMD module."""
+    logger = logging.getLogger('emd')
+    for handler in logger.handlers:
+        if handler.get_name() == 'console':
+            return handler.level
 
-    Note, this  doesn't remove the logger altogether, but sets the logging
-    level extremely high so no output ever appears.
-    """
 
-    logging.info('EMD logging shutting down')
-    logging.getLogger().setLevel(100)
+def set_format(formatter='', handler_name='console', prefix=''):
+    """Set new formatter EMD module logger."""
+    logger = logging.getLogger('emd')
+    new_config = yaml.load(default_config, Loader=yaml.FullLoader)
+    try:
+        fmtstr = new_config['formatters'][formatter]['format']
+    except KeyError:
+        logger.warning("EMD logger format type '{0}' not recognised".format(formatter))
+        raise KeyError("EMD logger format type '{0}' not recognised".format(formatter))
+    fmt = logging.Formatter(fmtstr.format(prefix=prefix))
+    for handler in logger.handlers:
+        if handler.get_name() == handler_name:
+            handler.setFormatter(fmt)
+    logger.info('EMD logger: handler {0} format changed to {1}'.format(handler_name, formatter))
+
+
+def disable():
+    """Turn off logging for the EMD module."""
+    logger = logging.getLogger('emd')
+    logger.info('EMD logging disabled')
+    logging.disable(sys.maxsize)
+
+
+def enable():
+    """Turn on logging for the EMD module."""
+    logger = logging.getLogger('emd')
+    logging.disable(logging.NOTSET)
+    logger.info('EMD logging enabled')
 
 
 # ------------------------------------
