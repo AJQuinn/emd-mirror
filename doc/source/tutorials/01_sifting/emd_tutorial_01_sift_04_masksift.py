56f9221fcc3ba39cd29b9c119d36d610aaf05396
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2021-02-19 15:08:49 +0000

    Update to mask sift tutorials

--- doc/source/tutorials/01_sifting/emd_tutorial_01_sift_04_masksift.py
+++ doc/source/tutorials/01_sifting/emd_tutorial_01_sift_04_masksift.py
@@ -33,11 +33,14 @@ xy = xx+yy
 # Make a quick summary plot
 plt.figure()
 plt.subplot(311)
-plt.plot(xy)
+plt.plot(yy)
+plt.legend(['Slow Signal'])
 plt.subplot(312)
 plt.plot(xx)
+plt.legend(['Transient Fast Signal'])
 plt.subplot(313)
-plt.plot(yy)
+plt.plot(xy)
+plt.legend(['Full Signal'])
 
 # sphinx_gallery_thumbnail_number = 2
 
@@ -53,6 +56,7 @@ imf = emd.sift.sift(xy, max_imfs=3)
 emd.plotting.plot_imfs(imf, cmap=True, scale_y=True)
 
 #%%
+#
 # The signals are well separated when both oscillations are present. However in
 # time periods where the fast 25Hz signal disappears the slower signal jumps up
 # to become part of the fast component. We'd prefer the separation into narrow
@@ -64,21 +68,34 @@ emd.plotting.plot_imfs(imf, cmap=True, scale_y=True)
 # part of the signal even if the definition of 'fast' might be different in
 # different segments.
 #
-# The Masked sift is a potential solution to this problem. This is a simple
+# The masked sift is a potential solution to this problem. This is a simple
 # trick which effectively puts a lower bound on the frequency content that can
 # enter a particular IMF. We will add a known masking signal to our time-series
 # before running ``emd.sift.get_next_imf``. Any signals which are lower in
 # frequency than this mask should then be ignored by the sift in favour of this
 # known signal. Finally, we can remove the known mask to recover our IMF.
 #
+
+#%%
+# Adding a single mask
+#^^^^^^^^^^^^^^^^^^^^^
+
+#%%
 # Here we make a 30Hz mask and plot it next to a segment of our time-series.
 
 mask = 0.1*np.sin(2*np.pi*30*time_vect)
 
-plt.figure()
+plt.figure(figsize=(10, 6))
+plt.subplot(121)
 plt.plot(xy)
 plt.plot(mask)
+plt.legend(['Signal', 'Mask'])
 plt.xlim(0, 1000)
+plt.subplot(122)
+plt.plot(xy + mask, color='k')
+plt.xlim(0, 1000)
+plt.legend(['Signal + Mask'])
+
 
 #%%
 # We see that the masking signal is close in frequency to the fast burst but
@@ -136,33 +153,73 @@ plt.title('Masked IMF with Mask removed')
 # adding masks. We can specify the frequency and amplitude of the mask to be
 # applied whilst isolating our IMF.
 #
+
+#%%
+# Choosing mask frequency
+#^^^^^^^^^^^^^^^^^^^^^^^^
+
+#%%
 # It is important that the mask frequency is approximately equal to the signal
 # component we want to isolate. If we use a mask of too high or too low
 # frequency then the procedure will not work.
 #
-# Next we use a mask with an very high frequency which suppresses both signal components.
+# Here we apply a range of masks to illustrate this effect. We apply a high
+# frequency mask, a low frequency mask and a 'just-right' mask close to the
+# 30Hz signal.
 
 # Masks should be specified in normalised frequencies between 0 and .5 where 0.5 is half the sampling rate
 high_mask_freq = 150/sample_rate
 imf_high_mask, _ = emd.sift.get_next_imf_mask(xy, high_mask_freq, 2)
 
-plt.figure()
-plt.plot(imf_high_mask)
-
-#%%
-# Finally a very low frequency mask which allows both components back through...
+med_mask_freq = 30/sample_rate
+imf_med_mask, _ = emd.sift.get_next_imf_mask(xy, med_mask_freq, 2)
 
 low_mask_freq = 2/sample_rate
 imf_low_mask, _ = emd.sift.get_next_imf_mask(xy, low_mask_freq, 2)
 
-
 plt.figure()
+plt.subplot(311)
+plt.plot(imf_high_mask)
+plt.ylim(-1, 1)
+plt.subplot(312)
+plt.plot(imf_med_mask)
+plt.ylim(-1, 1)
+plt.subplot(313)
 plt.plot(imf_low_mask)
+plt.ylim(-1, 1)
 
 #%%
-# ``emd.sift.mask_sift`` uses ``emd.sift.get_next_imf_mask`` internally to run
-# a whole set of sifts using the masking method. Each IMF is isolated with a
-# separate mask which decreases in frequency for each successive IMF.
+# We can see that the high frequency mask has suppressed both signal components
+# whist the low frequency mask has allowed them both through. In contrast, when
+# the mask frequency is closest to the fastest dynamics present in the signal -
+# it is able to successfully isolate them.
+
+#%%
+# Choosing a mask frequency may not be so simple in real data applications
+# where multiple signal components may be present alongside measurement noise.
+# We recommend a rule-of-thumb based on simulations in Fosso & Molinas 2017
+# (http://arxiv.org/abs/1709.05547). A given mask with frequency f Hz, can be
+# expected to remove frequencies below  0.7*f Hz. As an example, a mask with
+# frequency 50Hz will keep oscillatory content of 40Hz but remove content of
+# 30Hz.
+#
+# The flexibility provided by masked sifting overcomes several issue with
+# convential sifting but this flexibility can also lead to problems. If you do
+# use a highly tuned set of masks for an analysis, we recommend careful
+# checking of the IMFs before proceeding to the main analysis. You should
+# ensure that the IMFs are well separated - the components should have low
+# correlations/orthogonality scores - and that the instantaneous frequency
+# content of the IMFs do not strongly overlap.
+
+#%%
+# Running a full mask sift
+#^^^^^^^^^^^^^^^^^^^^^^^^^
+
+#%%
+# Until now, we've only been separating two signals with a single pass of the
+# sift. ``emd.sift.mask_sift`` uses ``emd.sift.get_next_imf_mask`` internally
+# to run a whole set of sifts using the masking method. Each IMF is isolated
+# with a separate mask which decreases in frequency for each successive IMF.
 #
 # Here we run a ``mask_sift`` using mask frequencies starting at 30Hz. This
 # will reduce by one half for each successive IMF - the second mask will be
