eb252679e5bb53669b61f7b46a98a5d17cbff219
marcoFabus <marco.fabus@ndcn.ox.ac.uk>
2021-02-26 11:15:57 +0000

    Added scaling for signal and integer ticks only for scale_y=True.

--- emd/plotting.py
+++ emd/plotting.py
@@ -50,17 +50,21 @@ def plot_imfs(imfs, time_vect=None, sample_rate=1, scale_y=False, freqs=None, cm
         time_vect = np.linspace(0, imfs.shape[0]/sample_rate, imfs.shape[0])
 
     mx = np.abs(imfs).max()
+    mx_sig = np.abs(imfs.sum(axis=1)).max()
 
     if fig is None:
         fig = plt.figure()
 
     ax = fig.add_subplot(nplots, 1, 1)
+    if scale_y:
+        ax.yaxis.get_major_locator().set_params(integer=True)
     for tag in ['top', 'right', 'bottom']:
         ax.spines[tag].set_visible(False)
     ax.plot((time_vect[0], time_vect[-1]), (0, 0), color=[.5, .5, .5])
     ax.plot(time_vect, imfs.sum(axis=1), 'k')
     ax.tick_params(axis='x', labelbottom=False)
     ax.set_xlim(time_vect[0], time_vect[-1])
+    ax.set_ylim(-mx_sig* 1.1, mx_sig * 1.1)
     ax.set_ylabel('Signal', rotation=0, labelpad=10)
 
     if cmap is True:
@@ -83,6 +87,7 @@ def plot_imfs(imfs, time_vect=None, sample_rate=1, scale_y=False, freqs=None, cm
         ax.set_xlim(time_vect[0], time_vect[-1])
         if scale_y:
             ax.set_ylim(-mx * 1.1, mx * 1.1)
+            ax.yaxis.get_major_locator().set_params(integer=True)
         ax.set_ylabel('IMF {0}'.format(ii), rotation=0, labelpad=10)
 
         if ii < nplots - 1:
