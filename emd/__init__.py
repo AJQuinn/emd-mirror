0c550eea1f0140cc3eb980da3d34821a6f1b9238
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2021-03-02 15:45:29 +0000

    Major overhaul of Cycles class and addition of private _cycles_support module

--- emd/__init__.py
+++ emd/__init__.py
@@ -8,5 +8,6 @@ from . import plotting  # noqa: F401, F403
 from . import example  # noqa: F401, F403
 from . import logger  # noqa: F401, F403
 from . import cycles  # noqa: F401, F403
+from . import _cycles_support  # noqa: F401, F403
 from . import sift  # noqa: F401, F403
 from . import support  # noqa: F401, F403
